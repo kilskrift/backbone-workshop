43c1e3f9b01c431dcdf8edd33fcfff8882024390
jocke <joakimber@gmail.com>
2013-02-27 15:22:48 +0100

    my_mysqlpw fix

--- api/index.php
+++ api/index.php
@@ -98,11 +98,11 @@ function deleteTodo($id) {
 	}
 }
 
-
 function getConnection() {
+    include('my_mysqlpw.php');
 	$dbhost="127.0.0.1";
 	$dbuser="root";
-	$dbpass="abc123";
+	$dbpass=$my_mysqlpw;
 	$dbname="todos";
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
